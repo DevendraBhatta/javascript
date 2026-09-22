0d052019a3743d8997bec986dfe0734f72afd0af
DevendraBhatta <devendrabhatta63@gmail.com>
2026-09-22 17:59:26 +0545

    small change

--- 01-calculator/assets/scripts/app.js
+++ 01-calculator/assets/scripts/app.js
@@ -1,1 +1,6 @@
-alert('This Works!');
+let currentResult = 0; //unintialized variable
+
+currentResult = (currentResult+10)*3/2-1;
+
+let calulationDesc  = '(currentResult+10)*3/2-1'
+outputResult(currentResult,calulationDesc);
